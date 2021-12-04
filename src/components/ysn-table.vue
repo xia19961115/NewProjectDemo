9beadda6756bc3baa32da303e94cf09b0e66aea9
夏宁 <478084629@qq.com>
2021-12-04 15:35:22 +0800

    x

--- src/components/ysn-table.vue
+++ src/components/ysn-table.vue
@@ -1,62 +1,117 @@
 <template>
-    <div>
-      <el-table
-        :data="compileData"
-        @selection-change="handleSelection"
-      >
+  <div>
+    <el-table :data="compileData" @selection-change="handleSelection" border>
       <!-- 是否需要筛选 -->
-        <el-table-column
-          v-if="selection"
-          type="selection"
-          width="55">
-        </el-table-column>
-        <el-table-column
-          v-for="(item,index) in options"
-          :key="index"
-          :prop="item.prop"
-          :label="item.label"
-          v-bind="{...$attrs,...item}"
-        >
-         <template slot-scope="scope">
-             <div>{{ scope.row[item.prop] }}</div>
-         </template>
-
-        </el-table-column>
-        
-      </el-table>
-    </div>
+      <el-table-column v-if="selection" type="selection" width="55">
+      </el-table-column>
+      <el-table-column
+        v-for="(item, index) in options"
+        :key="index"
+        :prop="item.prop"
+        :label="item.label"
+        v-bind="{ ...$attrs, ...item }"
+      >
+        <template slot-scope="scope">
+          <!-- 非handle 为非操作 -->
+          <template v-if="item.type !== 'handle'">
+            <!-- 根据业务场景来做判断 -->
+            <!-- 自己加 -->
+            <!-- 默认渲染数据 -->
+            <div>{{ scope.row[item.prop] }}</div>
+          </template>
+          <template v-else>
+            <div style="display: flex">
+              <div v-for="(item, index) in item.child" :key="index">
+                <!-- 二次确认 -->
+                <template v-if="item.popconfirm">
+                  <el-popconfirm
+                    :title="`确认${item.label}吗？`"
+                    confirm-button-text="确认"
+                    cancel-button-text="取消"
+                    @confirm="handleEmit(item.prop, scope.row, scope.$index)"
+                  >
+                    <template v-if="item.rule">
+                      <el-button
+                        v-if="scope.row[item.rule.prop] === item.rule.value"
+                        v-bind="{ ...$attrs, ...item }"
+                        @click="handleEmit(item.prop, scope.row, scope.$index)"
+                        slot="reference"
+                      >
+                        {{ item.label }}
+                      </el-button>
+                    </template>
+                    <template v-else>
+                      <el-button
+                        v-bind="{ ...$attrs, ...item }"
+                        @click="handleEmit(item.prop, scope.row, scope.$index)"
+                        slot="reference"
+                      >
+                        {{ item.label }}
+                      </el-button>
+                    </template>
+                  </el-popconfirm>
+                </template>
+                <!-- 非二次确认 -->
+                <template v-else>
+                  <!-- 规则判断 -->
+                  <template v-if="item.rule">
+                    <el-button
+                      v-if="scope.row[item.rule.prop] === item.rule.value"
+                      v-bind="{ ...$attrs, ...item }"
+                      @click="handleEmit(item.prop, scope.row, scope.$index)"
+                    >
+                      {{ item.label }}
+                    </el-button>
+                  </template>
+                  <template v-else>
+                    <el-button
+                      v-bind="{ ...$attrs, ...item }"
+                      @click="handleEmit(item.prop, scope.row, scope.$index)"
+                    >
+                      {{ item.label }}
+                    </el-button>
+                  </template>
+                </template>
+              </div>
+            </div>
+          </template>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 <script>
 export default {
-    name:'YsnTable',
-    props:{
-        data:{
-            type: Array,
-            default:() => []
-        },
-        options:{
-            type: Array,
-            default:() => []
-        },
-        selection:{
-            typeof:Boolean,
-            default:false
-        }
+  name: "YsnTable",
+  props: {
+    data: {
+      type: Array,
+      default: () => [],
     },
-    data(){
-        return{
-
-        }
+    options: {
+      type: Array,
+      default: () => [],
     },
-    computed: {
-      compileData() {
-          return this.data
-      }  
+    selection: {
+      typeof: Boolean,
+      default: false,
     },
-    methods:{
-        handleSelection(param) {
-            this.$emit('selection', param)
-        }
-    }
-}
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    compileData() {
+      return this.data;
+    },
+  },
+  methods: {
+    handleSelection(param) {
+      this.$emit("selection", param);
+    },
+    handleEmit(emit, item, index) {
+      this.$emit(emit, item, index);
+    },
+  },
+};
 </script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <ysn-table :data="data" :options="options"></ysn-table>
+    <ysn-table
+      :data="data"
+      :options="options"
+      :selection="true"
+      @edit="handleEdit"
+    ></ysn-table>
   </div>
 </template>
 <script>
@@ -22,7 +27,7 @@ export default {
         },
         {
           date: "2016-05-01",
-          name: "王小虎",
+          name: "王小虎1",
           address: "上海市普陀区金沙江路 1519 弄",
         },
         {
@@ -35,21 +40,48 @@ export default {
         {
           label: "日期",
           prop: "date",
-          align:'center',
-          width:'100px',
-          showOverflowTooltip:true
+          align: "center",
+          width: "100px",
+          showOverflowTooltip: true,
         },
         {
           label: "姓名",
           prop: "name",
-          formatter:(row)=>{ return row }
         },
         {
           label: "地址",
           prop: "address",
         },
+        {
+          label: "操作",
+          type: "handle",
+          child: [
+            {
+              label: "编辑",
+              type: "warning",
+              prop: "edit",
+              size: "mini",
+              popconfirm:true,
+              rule: {
+                prop: "name",
+                value: "王小虎1",
+              },
+            },
+            {
+              label: "详情",
+              type: "warning",
+              prop: "detail",
+              size: "mini",
+            },
+          ],
+        },
       ],
     };
+  },
+  methods: {
+    handleEdit(param) {
+      console.log(param.date);
+    },
   },
 };
 </script>
